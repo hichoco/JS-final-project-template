939a5fbcbd4e5b7842e7197fd656c86b6fef005f
hichoco <0958820398>
2017-02-04 15:25:39 +0800

    敵人趴趴走

--- main.js
+++ main.js
@@ -1,34 +1,43 @@
-
+var FPS = 70;
 var bgImg = document.createElement("img");
 var eImg = document.createElement("img");
 var bgim = document.createElement("img");
-var tower = document.createElement("img");
+var towerImg = document.createElement("img");
 
 bgImg.src = "images/map.png";
 eImg.src = "images/jason.gif";
 bgim.src = "images/tower-btn.png";
-tower.src = "images/tower.png";
+towerImg.src = "images/tower.png";
 
 var canvas = document.getElementById("gamecanvas");
 
 var ctx = canvas.getContext("2d");
 
 function draw() {
- 
-ctx.drawImage(bgImg,0,0);
-ctx.drawImage(eImg,enemy.x,enemy.y);
-ctx.drawImage(bgim,640-64,480-64,64,64);
-if (build == true) {
-ctx.drawImage(tower,move.x,move.y);
-}
+ enemy.imove()
+	ctx.drawImage(bgImg,0,0);
+	ctx.drawImage(eImg,enemy.x,enemy.y);
+	ctx.drawImage(bgim,640-64,480-64,64,64);
+	if (build == true) {
+		ctx.drawImage(towerImg,move.x,move.y);
+
+	}else{
+ 	ctx.drawImage(towerImg,tower.x,tower.y);
+	}
 
 }
 
-setInterval( draw, 40)
+setInterval( draw, 1000/FPS)
 
 var enemy={
 	x: 100,
-	y: 450
+	y: 450,
+	speedX:0,
+	speedY: -64,
+	imove: function(){
+		this.x = this.x + this.speedX/FPS;
+		this.y = this.y + this.speedY/FPS;
+	}
 }
 
 $("#gamecanvas").on("mousemove",mouse)
@@ -40,6 +49,10 @@ function mouse (event){
 }
 var move={x:300, y:100}
 var build =false
+var tower = {
+	x : 0,
+    y : 0,	
+}
 
 $("#gamecanvas").on("click",isbuild)
 
@@ -47,6 +60,11 @@ function isbuild (event){
 	if (event.offsetX>576 &&event.offsetY>416) {
 		build = true
 	}else{
+		if (build == true) {
+			tower.x = move.x - move.x%32
+			tower.y = move.y - move.y%32
 		build = false
+		
+		}
 	}
 }
