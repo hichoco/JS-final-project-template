781d4039e9e62e0200f28297b95930eea286cd87
hichoco <0958820398>
2017-03-17 20:53:15 +0800

    瞄準

--- main.js
+++ main.js
@@ -1,21 +1,24 @@
 var clock =0
 
-
+var hp = 100
 
 var FPS = 70;
 var bgImg = document.createElement("img");
 var eImg = document.createElement("img");
 var bgim = document.createElement("img");
 var towerImg = document.createElement("img");
+var shootImg = document.createElement("img");
 
 bgImg.src = "images/kk.png";
 eImg.src = "images/jason.gif"
 bgim.src = "images/tower-btn.png";
-towerImg.src = "images/tower.png";
+towerImg.src = "images/daigh.gif";
+shootImg.src = "images/crosshair.png";
 
 var canvas = document.getElementById("gamecanvas");
 
 var ctx = canvas.getContext("2d");
+
 function draw() {
 	clock++;
 	if ((clock%80)==0) {
@@ -26,9 +29,15 @@ function draw() {
 		
 	ctx.drawImage(bgImg,0,0);
 	for (var i = 0; i < enemies.length; i++) {
-		enemies[i].imove();
-		ctx.drawImage(eImg,enemies[i].x,enemies[i].y);
+		if (enemies[i].Hp <= 0) {
+			enemies.splice(i,1);
+		}else{
+			enemies[i].imove();
+			ctx.drawImage(eImg,enemies[i].x,enemies[i].y);
+		}
 	}
+	tower.searchEnemy()
+
 	ctx.drawImage(bgim,640-64,480-64,64,64);
 	if (build == true) {
 		ctx.drawImage(towerImg,move.x,move.y);
@@ -36,9 +45,16 @@ function draw() {
 	}else{
  	ctx.drawImage(towerImg,tower.x,tower.y);
 	}
-
+	ctx.font = "35px Arial";
+	ctx.fillStyle = "blue";
+	ctx.fillText("Hp:" + hp,32,30);
+	if (tower.aimingenemyID != null) {
+		var id = tower.aimingenemyID
+		ctx.drawImage(shootImg,enemies[id].x,enemies[id].y)
+	}
 }
-
+	
+	
 
 setInterval( draw, 1000/FPS)
 
@@ -55,6 +71,7 @@ var enemypath = [
 function Enemy(){
 	this.x = 96,
 	this.y = 448,
+	this.Hp = 10;
 	this.speedX = 0,
 	this.speedY = -64,
 	this.pathDes = 0,
@@ -106,11 +123,25 @@ function mouse (event){
 }
 var move={x:300, y:100}
 var build =false
+
 var tower = {
 	x : 0,
-    y : 0,	
-}
+    y : 0,
+    range : 100,
+    aimingenemyID: null,
+	searchEnemy: function(){
+		for(var i=0; i<enemies.length; i++){	
+	 		var distance = Math.sqrt(Math.pow(this.x-enemies[i].x,2) + Math.pow(this.y-enemies[i].y,2));
+			if (distance<=this.range) {
+				this.aimingenemyID = i;
+				return;
+			}
+ 		}
+	    // 如果都沒找到，會進到這行，清除鎖定的目標
+ 		this.aimingenemyID = null;
+	}
 
+}
 
 
 $("#gamecanvas").on("click",isbuild)
